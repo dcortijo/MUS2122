919122667527811ee8a6dfb6d5110f155d983a2e
Daniel Cortijo <dcortijo@ucm.es>
2022-03-03 15:11:51 +0100

    Ya arreglaremos el theremin

--- hoja4/ej3.py
+++ hoja4/ej3.py
@@ -1,5 +1,5 @@
 # EJ3: THEREMIN
-
+# Daniel Cortijo Gamboa & Tatiana Duarte Balvís
 import kbhit, os
 import sounddevice as sd   # modulo de conexion con portAudio
 import numpy as np
@@ -9,55 +9,8 @@ from pygame.locals import *
 
 RATE = 44100       # sampling rate, Hz, must be integer
 CHUNK = 16
-WIDTH = 100
+WIDTH = 500
 HEIGHT = 500
-
-
-class OscWaveTable:
-    def __init__(self, frec, vol, size):
-        self.frec = frec
-        self.vol = vol
-        self.size = size
-        # un ciclo completo de seno en [0,2pi)
-        t = np.linspace(0, 1, num=size)
-        self.waveTable = np.sin(2 * np.pi * t)
-        # arranca en 0
-        self.fase = 0
-        # paso en la wavetable en funcion de frec y RATE
-        self.step = self.size/(RATE/self.frec)
-
-    def setFrec(self,frec): 
-        self.frec = frec
-        self.step = self.size/(RATE/self.frec)
-
-    def getFrec(self): 
-        return self.frec    
-
-    def getChunk(self):
-        samples = np.zeros(CHUNK,dtype=np.float32)
-        cont = 0
-        #print("RATE ",RATE, "   frec ",self.frec)
-        
-        while cont < CHUNK:
-            self.fase = (self.fase + self.step) % self.size
-
-            # con truncamiento, sin redondeo
-            # samples[cont] = self.waveTable[int(self.fase)]
-
-            # con redondeo
-            #x = round(self.fase) % self.size
-            #samples[cont] = self.waveTable[x]
-                        
-            # con interpolacion lineal                                    
-            x0 = int(self.fase) % self.size
-            x1 = (x0 + 1) % self.size
-            y0, y1 = self.waveTable[x0], self.waveTable[x1]            
-            samples[cont] = y0 + (self.fase-x0)*(y1-y0)/(x1-x0)
-
-            cont = cont+1       
-
-        return self.vol*samples
-
 
 # [(fc,vol),(fm1,beta1),(fm2,beta2),...]
 def oscFM(frecs,frame):
@@ -86,7 +39,7 @@ stream.start()
 kb = kbhit.KBHit()
 c = ' '
 
-fc, fm = 220, 220
+fc, fm = 100, 100
 frecs = [[fc,0.8],[fc+fm,0.5],[fc+2*fm,0.3],[fc+3*fm,0.2]]
 frecRange = [100, 10000]
 amplitud = [0, 1]
